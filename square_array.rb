ec6113e9b5df91326f5532e008398e6aff5d9f3e
James <james.michalec58@gmail.com>
2021-01-14 14:21:52 -0800

    commit tree 1dfa747aebd194ef2b5fe91afeeb204e48c74b06

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
 def square_array(array)
-  # your code here
+  squared_number = 0;
+  squared_number_array = []
+  array.each do |array_number|
+    squared_number = array_number ** 2
+    squared_number_array.push(squared_number)
+  end
+  squared_number_array
+
 end
